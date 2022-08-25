9a0d176e4e0aed947e8508d812bdeaf01e8cf05e
daghan <daghan.altunsoy@gmail.com>
2022-08-25 21:50:28 +0300

    first commit

--- aws_lambda_dynamodb/main.tf
+++ aws_lambda_dynamodb/main.tf
@@ -24,24 +24,10 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket        = "${var.bucket_name}-${data.aws_caller_identity.aws_id.account_id}"
-  force_destroy = true
-  
-  tags = {
-    Name        = "App bucket"
-    Environment = "Dev"
-  }
-}
-
 resource "aws_iam_role_policy" "dynamodb-lambda-policy" {
    name = "dynamodb_lambda_policy"
    role = aws_iam_role.lambda_exec.id
+   
    policy = jsonencode({
       "Version" : "2012-10-17",
       "Statement" : [
@@ -60,6 +46,21 @@ resource "aws_iam_role_policy" "dynamodb-lambda-policy" {
         }
       ]
    })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket        = "${var.bucket_name}-${data.aws_caller_identity.aws_id.account_id}"
+  force_destroy = true
+  
+  tags = {
+    Name        = "App bucket"
+    Environment = "Dev"
+  }
 }
 
 resource "aws_s3_object" "lambda_app" {
