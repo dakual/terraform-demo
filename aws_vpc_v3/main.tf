73878e47a3962634cc71401f2e6a7e8f8ab8288f
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2022-11-22 15:31:40 +0300

    first commit

--- aws_vpc_v3/main.tf
+++ aws_vpc_v3/main.tf
@@ -31,161 +31,161 @@ module "vpc" {
   availability_zones  = ["eu-central-1a", "eu-central-1b"]
 }
 
-resource "aws_security_group" "main" {
-  name        = "${local.name}-sg"
-  description = "Allow all inbound and outbound traffic"
-  vpc_id      = module.vpc.id
+# resource "aws_security_group" "main" {
+#   name        = "${local.name}-sg"
+#   description = "Allow all inbound and outbound traffic"
+#   vpc_id      = module.vpc.id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-# APPLICATION LOADBALANCER
-resource "aws_lb" "main" {
-  name               = "${local.name}-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.main.id]
-  subnets            = module.vpc.public_subnets.*.id
-}
-
-resource "aws_alb_target_group" "main" {
-  name        = "${local.name}-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = module.vpc.id
-  target_type = "ip"
-
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
-}
-
-resource "aws_alb_listener" "http" {
-  load_balancer_arn = aws_lb.main.id
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = aws_alb_target_group.main.id
-    type = "forward"
-  }
-}
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 
-# AIM ROLE
-data "aws_iam_policy_document" "ecs_tasks_execution_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
+# # APPLICATION LOADBALANCER
+# resource "aws_lb" "main" {
+#   name               = "${local.name}-alb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.main.id]
+#   subnets            = module.vpc.public_subnets.*.id
+# }
 
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
+# resource "aws_alb_target_group" "main" {
+#   name        = "${local.name}-tg"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = module.vpc.id
+#   target_type = "ip"
 
-resource "aws_iam_role" "ecs_tasks_execution_role" {
-  name = "${local.name}-ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
-}
+#   health_check {
+#     healthy_threshold   = "3"
+#     interval            = "30"
+#     protocol            = "HTTP"
+#     matcher             = "200"
+#     timeout             = "3"
+#     path                = "/"
+#     unhealthy_threshold = "2"
+#   }
+# }
 
-resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
-  role       = aws_iam_role.ecs_tasks_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+# resource "aws_alb_listener" "http" {
+#   load_balancer_arn = aws_lb.main.id
+#   port              = 80
+#   protocol          = "HTTP"
 
-# ECS CLUSTER, SERVICE AND TASK
-resource "aws_cloudwatch_log_group" "task" {
-  name = "/ecs/${local.name}-task"
-}
+#   default_action {
+#     target_group_arn = aws_alb_target_group.main.id
+#     type = "forward"
+#   }
+# }
 
-resource "aws_ecs_cluster" "main" {
-  name = "${local.name}-cluster"
-}
 
-resource "aws_ecs_task_definition" "main" {
-  family                   = "${local.name}-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
-  execution_role_arn       = aws_iam_role.ecs_tasks_execution_role.arn
+# # AIM ROLE
+# data "aws_iam_policy_document" "ecs_tasks_execution_role" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
 
-  container_definitions = jsonencode([{
-      name  = "${local.name}-container"
-      image = "${local.container_image}"
-      essential = true
-      environment = [{
-          name  = "ENV_MESSAGE"
-          value = "DEMO APP"
-      }]
-      portMappings = [{
-          protocol = "tcp"
-          containerPort = local.container_port
-          hostPort = local.container_port
-      }]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.task.name
-          awslogs-region        = local.region
-          awslogs-stream-prefix = "ecs"
-        }
-      }
-  }])
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ecs-tasks.amazonaws.com"]
+#     }
+#   }
+# }
 
-  runtime_platform {
-    operating_system_family = "LINUX"
-    cpu_architecture        = "X86_64"
-  }
-}
+# resource "aws_iam_role" "ecs_tasks_execution_role" {
+#   name = "${local.name}-ecsTaskExecutionRole"
+#   assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
+# }
 
-resource "aws_ecs_service" "main" {
-  name                               = "${local.name}-service"
-  cluster                            = aws_ecs_cluster.main.id
-  task_definition                    = aws_ecs_task_definition.main.arn
-  desired_count                      = 1
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
-  health_check_grace_period_seconds  = 60
-  launch_type                        = "FARGATE"
-  scheduling_strategy                = "REPLICA"
+# resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+#   role       = aws_iam_role.ecs_tasks_execution_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
 
-  network_configuration {
-    security_groups  = [aws_security_group.main.id]
-    subnets          = module.vpc.private_subnets.*.id
-    assign_public_ip = false
-  }
+# # ECS CLUSTER, SERVICE AND TASK
+# resource "aws_cloudwatch_log_group" "task" {
+#   name = "/ecs/${local.name}-task"
+# }
 
-  load_balancer {
-    target_group_arn = aws_alb_target_group.main.arn
-    container_name   = "${local.name}-container"
-    container_port   = local.container_port
-  }
+# resource "aws_ecs_cluster" "main" {
+#   name = "${local.name}-cluster"
+# }
 
-  lifecycle {
-    ignore_changes = [task_definition, desired_count]
-  }
-}
+# resource "aws_ecs_task_definition" "main" {
+#   family                   = "${local.name}-task"
+#   requires_compatibilities = ["FARGATE"]
+#   network_mode             = "awsvpc"
+#   cpu                      = 256
+#   memory                   = 512
+#   execution_role_arn       = aws_iam_role.ecs_tasks_execution_role.arn
+
+#   container_definitions = jsonencode([{
+#       name  = "${local.name}-container"
+#       image = "${local.container_image}"
+#       essential = true
+#       environment = [{
+#           name  = "ENV_MESSAGE"
+#           value = "DEMO APP"
+#       }]
+#       portMappings = [{
+#           protocol = "tcp"
+#           containerPort = local.container_port
+#           hostPort = local.container_port
+#       }]
+#       logConfiguration = {
+#         logDriver = "awslogs"
+#         options = {
+#           awslogs-group         = aws_cloudwatch_log_group.task.name
+#           awslogs-region        = local.region
+#           awslogs-stream-prefix = "ecs"
+#         }
+#       }
+#   }])
+
+#   runtime_platform {
+#     operating_system_family = "LINUX"
+#     cpu_architecture        = "X86_64"
+#   }
+# }
+
+# resource "aws_ecs_service" "main" {
+#   name                               = "${local.name}-service"
+#   cluster                            = aws_ecs_cluster.main.id
+#   task_definition                    = aws_ecs_task_definition.main.arn
+#   desired_count                      = 1
+#   deployment_minimum_healthy_percent = 50
+#   deployment_maximum_percent         = 200
+#   health_check_grace_period_seconds  = 60
+#   launch_type                        = "FARGATE"
+#   scheduling_strategy                = "REPLICA"
+
+#   network_configuration {
+#     security_groups  = [aws_security_group.main.id]
+#     subnets          = module.vpc.private_subnets.*.id
+#     assign_public_ip = false
+#   }
+
+#   load_balancer {
+#     target_group_arn = aws_alb_target_group.main.arn
+#     container_name   = "${local.name}-container"
+#     container_port   = local.container_port
+#   }
+
+#   lifecycle {
+#     ignore_changes = [task_definition, desired_count]
+#   }
+# }
 
 output "lb_dns_name" {
   value = aws_lb.main.dns_name
